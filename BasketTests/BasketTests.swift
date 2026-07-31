9301dd89e08130784ad8314ef1bd7fe3b12644b7
chee <yay@chee.party>
2026-07-31 21:14:29 +0100

    Initial Commit

---
+++ BasketTests/BasketTests.swift
@@ -0,0 +1,18 @@
+//
+//  BasketTests.swift
+//  BasketTests
+//
+//  Created by chee on 2026-07-31.
+//
+
+import Testing
+
+struct BasketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
